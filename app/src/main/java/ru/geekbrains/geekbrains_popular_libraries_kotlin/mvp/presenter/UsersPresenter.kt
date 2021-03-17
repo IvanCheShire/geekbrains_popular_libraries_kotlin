96d5ced0e86ba3411dd9f207a9e700769e274663
IvanCheShire <ludvigkrauz6@gmail.com>
2021-03-17 14:39:14 +0300

    3rd lesson

--- app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/presenter/UsersPresenter.kt
+++ app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/presenter/UsersPresenter.kt
@@ -2,28 +2,28 @@ package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter
 
 import com.github.terrakok.cicerone.Router
 import moxy.MvpPresenter
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.GithubUsersRepo
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.entity.GithubUser
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter.list.IUsersListPresenter
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.repo.GithubUsersRepo
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.navigation.IScreens
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter.list.IUserListPresenter
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.UsersView
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.list.IUserItemView
+import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.list.UserItemView
 
-
-class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router) :
-    MvpPresenter<UsersView>() {
-
-    class UsersListPresenter : IUsersListPresenter {
+class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router, val screens: IScreens, val uiScheduler: Scheduler) : MvpPresenter<UsersView>() {
+    class UsersListPresenter : IUserListPresenter {
         val users = mutableListOf<GithubUser>()
-        override var itemClickListener: ((IUserItemView) -> Unit)? = null
+        override var itemClickListener: ((UserItemView) -> Unit)? = null
 
-        override fun bindView(view: IUserItemView) {
+        override fun getCount() = users.size
+
+        override fun bindView(view: UserItemView) {
             val user = users[view.pos]
             view.setLogin(user.login)
         }
-
-        override fun getCount() = users.size
     }
-
+    val compositeDisposable = CompositeDisposable()
     val usersListPresenter = UsersListPresenter()
 
     override fun onFirstViewAttach() {
@@ -31,21 +31,33 @@ class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router) :
         viewState.init()
         loadData()
 
-        usersListPresenter.itemClickListener = { view ->
-            val user = usersListPresenter.users[view.pos]
+        usersListPresenter.itemClickListener = { itemView ->
+            val user = usersListPresenter.users[itemView.pos]
             router.navigateTo(screens.user(user))
         }
     }
 
     fun loadData() {
-        val users = usersRepo.getUsers()
-        usersListPresenter.users.clear()
-        usersListPresenter.users.addAll(users)
-        viewState.updateList()
+        val disposable = usersRepo.getUsers()
+            .observeOn(uiScheduler)
+            .subscribe({
+                usersListPresenter.users.addAll(it)
+                viewState.updateList()
+            }, {
+                it.printStackTrace()
+            })
+
+        compositeDisposable.add(disposable)
     }
 
-    fun backClick(): Boolean {
+    fun backPressed(): Boolean {
         router.exit()
         return true
     }
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
+    }
+
 }
